86a197da7ce986711122cf3a8321d31584254a32
inuyashaaa <huymanhtmhp@gmail.com>
2017-06-21 17:01:09 +0700

    Update, edit, delete users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
-  def index
+  before_action :logged_in_user, except: [:new, :create, :show]
+  before_action :load_user, except: [:index, :new, :create]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :verify_admin, only: :destroy
 
+  def index
+    @users = User.select(:id, :name, :email, :is_admin)
+      .order(:name).page(params[:page]).per_page Settings.user.per_page
   end
 
   def new
@@ -11,10 +17,10 @@ class UsersController < ApplicationController
     @user = User.new user_params
 
     if @user.save
-      flash[:info] = t ".check_mail"
+      flash[:success] = t ".create_success"
       redirect_to root_url
     else
-      flash.now[:danger] = t ".error"
+      flash.now[:error] = t ".error"
       render :new
     end
   end
@@ -23,9 +29,47 @@ class UsersController < ApplicationController
     @user = User.find_by id: params[:id]
   end
 
+  def edit; end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t ".flash"
+      redirect_to @user
+    else
+      flash.now[:error] = t ".error"
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy ? (flash[:success] = t ".deleted") : (flash.now[:error] = t ".error")
+    redirect_to users_url
+  end
+
   private
 
   def user_params
     params.require(:user).permit :name, :email, :password, :password_confirmation
+  end
+
+  def correct_user
+    @user = User.find_by id: params[:id]
+
+    unless @user.is_user? current_user
+      flash[:error] = t ".error"
+      redirect_to root_url
+    end
+  end
+
+  def load_user
+    @user = User.find_by id: params[:id]
+
+    if @user.nil?
+      render file: "public/404.html", status: :not_found, layout: false
+    end
+  end
+
+  def verify_admin
+    redirect_to root_url unless current_user.is_admin?
   end
 end
